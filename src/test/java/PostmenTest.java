6d8c72b4fd45236463db3b7c48f0f2fef2fe9341
slet <slet113@mail.ru>
2023-04-18 15:09:19 +0300

    test failed

--- src/test/java/PostmenTest.java
+++ src/test/java/PostmenTest.java
@@ -18,7 +18,7 @@ public class PostmenTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 }
