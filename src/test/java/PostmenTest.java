55109ca556511999fd0bc7edd08d938e54503bd3
slet <slet113@mail.ru>
2023-04-18 15:11:40 +0300

    test again pass

--- src/test/java/PostmenTest.java
+++ src/test/java/PostmenTest.java
@@ -18,7 +18,7 @@ public class PostmenTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
